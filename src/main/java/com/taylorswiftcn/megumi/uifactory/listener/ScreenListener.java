bb157d8568135d086be2f26b53177abf422ce730
justwei <admin@taylorswiftcn.com>
2021-11-19 19:38:24 +0800

    [add] ScreenListener

--- src/main/java/com/taylorswiftcn/megumi/uifactory/listener/ScreenListener.java
+++ src/main/java/com/taylorswiftcn/megumi/uifactory/listener/ScreenListener.java
@@ -1,7 +1,10 @@
 package com.taylorswiftcn.megumi.uifactory.listener;
 
+import com.taylorswiftcn.megumi.uifactory.UIFactoryPlugin;
+import com.taylorswiftcn.megumi.uifactory.event.screen.UIFScreenCloseEvent;
 import com.taylorswiftcn.megumi.uifactory.event.screen.UIFScreenOpenEvent;
 import net.sakuragame.eternal.dragoncore.api.CoreAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,6 +16,15 @@ public class ScreenListener implements Listener {
         Player player = e.getPlayer();
         String screenID = e.getScreenID();
 
-        CoreAPI.addOpenScreen(player.getUniqueId(), screenID);
+        Bukkit.getScheduler().runTaskLater(UIFactoryPlugin.getInstance(), () -> {
+            CoreAPI.addOpenScreen(player.getUniqueId(), screenID);
+        }, 1);
+    }
+
+    @EventHandler
+    public void onClose(UIFScreenCloseEvent e) {
+        Player player = e.getPlayer();
+
+        CoreAPI.delOpenScreen(player.getUniqueId());
     }
 }
